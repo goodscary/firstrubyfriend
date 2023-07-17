ed7ee9955f5ba985a2570c15b50e3c0a57de6735
pratiktamang <pratiktamang13@gmail.com>
2023-07-17 13:06:42 +0100

    Fix tests + remove index path

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -18,7 +18,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should sign in" do
-    post sign_in_url, params: { email: @user.email, password: "Secret1*3*5*" }
+    post sign_in_url, params: {email: @user.email, password: "Secret1*3*5*"}
     assert_redirected_to root_url
 
     get root_url
@@ -26,21 +26,16 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not sign in with wrong credentials" do
-    post sign_in_url, params: { email: @user.email, password: "SecretWrong1*3" }
+    post sign_in_url, params: {email: @user.email, password: "SecretWrong1*3"}
     assert_redirected_to sign_in_url(email_hint: @user.email)
     assert_equal "That email or password is incorrect", flash[:alert]
-
-    get root_url
-    assert_redirected_to sign_in_url
   end
 
   test "should sign out" do
     sign_in_as @user
 
     delete session_url(@user.sessions.last)
-    assert_redirected_to sessions_url
-
-    follow_redirect!
-    assert_redirected_to sign_in_url
+    assert_equal "That session has been logged out", flash[:notice]
+    assert_redirected_to root_url
   end
 end
--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -4,6 +4,6 @@ class HomeTest < ActionDispatch::IntegrationTest
   test "GET /" do
     get "/"
 
-    assert_select "h1", text: "Hello"
+    assert_select "h1", text: "First Ruby Mentor"
   end
 end
