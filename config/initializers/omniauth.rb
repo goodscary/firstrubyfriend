184f35196b35a056334ec8f18052cfbf74f9120d
pratiktamang <pratiktamang13@gmail.com>
2023-07-17 13:47:52 +0100

    wip omniauth + github

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production? # You should replace it with your provider
+  provider :github, "clientid", "secret id", scope: "user:email"
 end
