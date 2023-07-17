e1850bbd5e81d095878db3fc06edb01ca9c07c8e
pratiktamang <pratiktamang13@gmail.com>
2023-07-17 12:09:02 +0100

    add logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root "home#show"
+  root "home#index"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  resources :sessions, only: [:index, :show, :destroy]
+  resource :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email, only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset, only: [:new, :edit, :create, :update]
+  end
 end
