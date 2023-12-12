47cb3c08b6f08b77959fc5d1f5bbaad33efdd920
pratiktamang <pratiktamang13@gmail.com>
2023-12-12 01:52:15 +0000

    initialise mentor_questionnaire controller for current form questions, add ulid to models and test

---
+++ app/helpers/mentor_questionnaires_helper.rb
@@ -0,0 +1,2 @@
+module MentorQuestionnairesHelper
+end
--- app/models/mentor_questionnaire.rb
+++ app/models/mentor_questionnaire.rb
@@ -1,4 +1,8 @@
 class MentorQuestionnaire < ApplicationRecord
+  include ULID::Rails
+  ulid :id, auto_generate: true
+  ulid :respondent_id
+
   belongs_to :respondent, class_name: "User", foreign_key: "respondent_id"
 
   validates :respondent_id, presence: true
