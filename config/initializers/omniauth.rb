2ca186cfbabee4abaef73f4af7e41ff4cbad6101
Andy Croll <andy@goodscary.com>
2025-01-08 18:41:29 +0000

    Store and use development Github Oauth credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_ID"], ENV["GITHUB_SECRET"]
+  provider :github, Rails.application.credentials.dig(:github, :key), Rails.application.credentials.dig(:github, :secret), scope: "user:email"
 end
