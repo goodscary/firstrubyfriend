4072c5255853df1d81f68ca810be81a39a17c773
Andy Croll <andy@goodscary.com>
2025-01-08 19:28:23 +0000

    Wire up and install stimulus js components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start();
+
+// Import and register all TailwindCSS Components or just the ones you need
+import { Alert, Autosave, ColorPreview, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+// application.register('alert', Alert)
+// application.register('autosave', Autosave)
+// application.register('color-preview', ColorPreview)
+application.register('dropdown', Dropdown)
+// application.register('modal', Modal)
+// application.register('popover', Popover)
+// application.register('slideover', Slideover)
+// application.register('tabs', Tabs)
+// application.register('toggle', Toggle)
+
 import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,8 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "tailwindcss-stimulus-components" # @6.1.3
+
 pin_all_from "app/javascript/controllers", under: "controllers"
